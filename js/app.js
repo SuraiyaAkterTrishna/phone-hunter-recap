7b5b607d6edae5f07ecad51c650980960885d4fb
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-05-15 12:25:57 +0600

    loader spinner toggle done

--- js/app.js
+++ js/app.js
@@ -11,13 +11,12 @@ const displayPhones = (phones) => {
   //   display only 10 phones
   phones = phones.slice(0, 10);
   // no phone message
-const noMessage = document.getElementById('no-message');
-if(phones.length === 0) {
-    noMessage.classList.remove('d-none');
-}
-else{
-    noMessage.classList.add('d-none');
-}
+  const noMessage = document.getElementById("no-message");
+  if (phones.length === 0) {
+    noMessage.classList.remove("d-none");
+  } else {
+    noMessage.classList.add("d-none");
+  }
   phones.forEach((phone) => {
     // console.log(phone);
     const phoneDiv = document.createElement("div");
@@ -33,10 +32,23 @@ else{
     `;
     phonesContainer.appendChild(phoneDiv);
   });
+//   stop loader
+toggleSpinner(false);
 };
 document.getElementById("btn-search").addEventListener("click", function () {
+  // start loader
+  toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   loadPhones(searchText);
 });
+// loading spinner
+const toggleSpinner = (isLoading) => {
+  const loaderSection = document.getElementById("loader");
+  if (isLoading) {
+    loaderSection.classList.remove("d-none");
+  } else {
+    loaderSection.classList.add("d-none");
+  }
+};
 // loadPhones();
